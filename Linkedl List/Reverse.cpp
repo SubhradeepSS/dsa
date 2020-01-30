af452df3fb034b34541082ce2e4d2962f4cd24ba
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-01-30 19:18:06 +0530

    Create Reverse.cpp

---
+++ Linkedl List/Reverse.cpp
@@ -0,0 +1,11 @@
+ListNode* reverseList(ListNode* head) {
+        ListNode *prev=NULL,*curr=head,*next;
+        while(curr){
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+        head=prev;
+        return head;
+    }
