c6c21ff4310779d82d8a5cf74ccb890a020119cc
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-03-20 20:40:53 +0530

    Update Dice Throw.py

--- DP/Dice Throw.py
+++ DP/Dice Throw.py
@@ -1,11 +1,12 @@
 def findWays(m,n,x):
+    p=10**9+7
     table=[[0]*(x+1) for i in range(n+1)]
     for i in range(1,min(m+1,x+1)):
         table[1][i]=1
     for i in range(2,n+1):
         for j in range(1,x+1):
             for k in range(1,min(m+1,j)):
-                table[i][j]+=table[i-1][j-k]
+                table[i][j]=(table[i-1][j-k]%p+table[i][j]%p)%p #For large numbers sum
     return table[n][x]
 
 print(findWays(4,2,1)) 
