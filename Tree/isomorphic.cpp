621ef8f39b1835ef361e4dafba2b4df72eebe25f
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-03-29 14:58:17 +0530

    Update isomorphic.cpp

--- Tree/isomorphic.cpp
+++ Tree/isomorphic.cpp
@@ -3,8 +3,8 @@ bool f(node* n1,node*n2){
     return true;
   if(n1!=NULL&&n2!=NULL)
     return false;
-  if(n1->val==n2->val)
-    return true;
+  if(n1->val!=n2->val)
+    return false;
   return ((f(n1->left,n2->left)&&f(n1->right,n2->right))||
           (f(n1->left,n2->right)&&f(n1->right,n2->left)));
 }
