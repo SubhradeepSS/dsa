0db30e261faa787adb0f1e5779336fa5a3312616
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-04-17 20:55:53 +0530

    Create count say.py

---
+++ String/count say.py
@@ -0,0 +1,39 @@
+"""
+The count-and-say sequence is the sequence of integers with the first five terms as following:
+
+1.     1
+2.     11
+3.     21
+4.     1211
+5.     111221
+1 is read off as "one 1" or 11.
+11 is read off as "two 1s" or 21.
+21 is read off as "one 2, then one 1" or 1211.
+
+Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the count-and-say sequence. You can do so recursively, in other words from the previous member read off the digits, counting the number of digits in groups of the same digit.
+
+Note: Each term of the sequence of integers will be represented as a string.
+"""
+class Solution(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        if n==1:
+            return '1'
+        if n==2:
+            return '11'
+        s='11'
+        for _ in range(n-2):
+            c=1
+            x=''
+            for i in range(len(s)-1):
+                if s[i]==s[i+1]:
+                    c+=1
+                else:
+                    x+=str(c)+s[i]
+                    c=1
+            x+=str(c)+s[-1]
+            s=x
+        return s
