b3226c06339cdb2b3b17e9e28fc3fee0b0e3cf52
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-04-21 10:06:19 +0530

    Update Jumps.cpp

--- DP/Jumps.cpp
+++ DP/Jumps.cpp
@@ -23,3 +23,16 @@ int main()
     cout << "Minimum number of jumps to reach end is " << minJumps(arr, size); 
     return 0; 
 } 
+
+//O(n) solution
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int first_good=nums.size()-1;
+        for(int i=nums.size()-1;i>=0;i--){
+            if(i+nums[i]>=first_good)
+                first_good=i;
+        }
+        return (first_good==0);
+    }
+};
