59077fb5421c96ef9213fb01a37969bbfad0f27d
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-04-21 15:58:36 +0530

    Update Leftmost Column with at Least a One.py

--- Binary Search/Leftmost Column with at Least a One.py
+++ Binary Search/Leftmost Column with at Least a One.py
@@ -101,3 +101,39 @@ class Solution(object):
             return ans
         return -1
             
+# Alternate Solution
+# """
+# This is BinaryMatrix's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class BinaryMatrix(object):
+#    def get(self, x, y):
+#        """
+#        :type x : int, y : int
+#        :rtype int
+#        """
+#
+#    def dimensions:
+#        """
+#        :rtype list[]
+#        """
+
+class Solution(object):
+    def leftMostColumnWithOne(self, binaryMatrix):
+        """
+        :type binaryMatrix: BinaryMatrix
+        :rtype: int
+        """
+        l=binaryMatrix.dimensions()
+        n,m=l[0],l[1]
+        
+        r,c=0,m-1
+        ans=-1
+        while r<n and c>=0:
+            if binaryMatrix.get(r,c)==1:
+                ans=c
+                c-=1
+            else:
+                r+=1
+        return ans
+            
