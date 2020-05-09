7a9a9c1d8b2b59c4ae1d2bd8c681505afbc111ab
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-05-09 15:15:03 +0530

    Create perfect squares.cpp

---
+++ DP/perfect squares.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        if(n<=3)
+            return n;
+        int dp[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=3;
+        for(int i=4;i<=n;i++){
+            dp[i]=i;
+            for(int x=1;x<=ceil(sqrt(i));x++){
+                int t=x*x;
+                if(t>i)
+                    break;
+                else
+                    dp[i]=min(dp[i],1+dp[i-t]);
+            }
+        }
+        return dp[n];
+    }
+};
