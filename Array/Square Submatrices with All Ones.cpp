5992a4e8524b151991f042601dfe0640b495e74c
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-05-22 11:11:41 +0530

    Create Square Submatrices with All Ones.cpp

---
+++ Array/Square Submatrices with All Ones.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        int m=matrix.size(),n=matrix[0].size();
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]&&matrix[i][j-1]&&matrix[i-1][j]&&matrix[i-1][j-1])
+                    matrix[i][j]=1+min(matrix[i][j-1],min(matrix[i-1][j],matrix[i-1][j-1]));
+            }
+        }
+        int ans=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                ans+=matrix[i][j];
+            }
+        }
+        return ans;
+    }
+};
