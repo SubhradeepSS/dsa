cd22312cf685ac6ec615796917104476a0beccda
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-05-30 20:22:46 +0530

    Create k closest points to origin.cpp

---
+++ Array/k closest points to origin.cpp
@@ -0,0 +1,26 @@
+bool f(pair<int,pair<int,int>> p1,pair<int,pair<int,int>> p2){
+        return p1.first<p2.first;
+    }
+class Solution {
+public:
+    int dist(int x1,int y1){
+        return x1*x1+y1*y1;
+    }
+    
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        int n=points.size();
+        vector<pair<int,pair<int,int>>> p(n);
+        for(int i=0;i<n;i++){
+            p[i].first=dist(points[i][0],points[i][1]);
+            p[i].second.first=points[i][0];
+            p[i].second.second=points[i][1];
+        }
+        sort(p.begin(),p.end(),f);
+        
+        vector<vector<int>> ans;
+        
+        for(int i=0;i<K;i++)
+            ans.push_back({p[i].second.first,p[i].second.second});
+        return ans;
+    }
+};
