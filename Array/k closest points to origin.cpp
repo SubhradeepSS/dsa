3301cc334433ac05cddf4f4328ecaa673a0c0595
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-05-30 20:26:52 +0530

    Update k closest points to origin.cpp

--- Array/k closest points to origin.cpp
+++ Array/k closest points to origin.cpp
@@ -24,3 +24,10 @@ public:
         return ans;
     }
 };
+
+// Python solution
+class Solution:
+    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
+        points.sort(key=lambda p:p[0]**2+p[1]**2)
+        return points[:K]
+        
