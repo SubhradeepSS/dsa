eb64ef34e651eb098ba374b883a7226468a2de1e
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-06-18 16:11:27 +0530

    Update delete node when head is not given.cpp

--- Linked List/delete node when head is not given.cpp
+++ Linked List/delete node when head is not given.cpp
@@ -2,3 +2,10 @@ void deleteNode(ListNode* node) {
         node->val=node->next->val;
         node->next=node->next->next;
     }
+
+// alternate
+void deleteNode(Node *node)
+{
+   // Your code here
+   *node=*(node->next);
+}
