3a26dd184fd31ef8a48bb3c90453c4bd8852e8ee
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-07-02 13:04:21 +0530

    Added reverse level order

--- Tree/Level order traversal.cpp
+++ Tree/Level order traversal.cpp
@@ -93,3 +93,36 @@ public:
         return v;
     }
 };
+
+
+// Reverse level order 
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> ans;
+        queue<TreeNode*> q;
+        
+        if(!root)
+            return ans;
+        
+        q.push(root);
+        
+        while(!q.empty()){
+            int s=q.size();
+            vector<int> curr;
+            while(s--){
+                TreeNode* f=q.front();
+                q.pop();
+                curr.push_back(f->val);
+                if(f->left)
+                    q.push(f->left);
+                if(f->right)
+                    q.push(f->right);
+            }
+            ans.push_back(curr);
+        }
+        
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
