6bc4d3fe8aacc0a9cba86c8db78ba0f18dc08546
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-07-03 18:45:18 +0530

    Update Prison cells after n days.cpp

--- Math/Prison cells after n days.cpp
+++ Math/Prison cells after n days.cpp
@@ -34,3 +34,41 @@ Note: cells.length == 8
 cells[i] is in {0, 1}
 1 <= N <= 10^9 
 */
+
+class Solution {
+public:
+    vector<int> prisonAfterNDays(vector<int>& cells, int N) {
+        vector<int> prev=cells;
+        const int n=8;
+        vector<int> ans(n);
+        
+        for(int i=0;i<min(14,N);i++){
+            ans[0]=0;
+                for(int i=1;i<n-1;i++){
+                    ans[i]=!(prev[i-1]^prev[i+1]);
+                }
+                ans[n-1]=0;
+                prev=ans;
+        }
+        if(N<14)
+            return ans;
+        
+        N%=14;
+        
+        while(N--){
+            // for(int i=0;i<n;i++)
+            //     cout<<prev[i]<<" ";
+            // cout<<endl;
+            
+            ans[0]=0;
+            for(int i=1;i<n-1;i++){
+                ans[i]=!(prev[i-1]^prev[i+1]);
+            }
+            ans[n-1]=0;
+            prev=ans;
+            
+        }
+        
+        return ans;
+    }
+};
