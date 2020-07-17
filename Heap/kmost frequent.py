35c0de7bd7045e2d976fedda5aba97d8de3a5f97
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-07-17 18:41:54 +0530

    Added C++ version

--- Heap/kmost frequent.py
+++ Heap/kmost frequent.py
@@ -1,3 +1,4 @@
+# Python
 class Solution(object):
     def topKFrequent(self, nums, k):
         """
@@ -8,3 +9,28 @@ class Solution(object):
         count=collections.Counter(nums)
         return heapq.nlargest(k,count.keys(),key=count.get)
         
+# Added C++ version
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        priority_queue<pair<int,int>> pq;
+        
+        unordered_map<int,int> mp;
+        for(int i:nums)
+            mp[i]++;
+        
+        vector<int> ans;
+        for(auto it:mp){
+            pq.push(make_pair(it.second,it.first));
+        }
+        
+        while(k--){
+            auto p=pq.top();
+            pq.pop();
+            
+            ans.push_back(p.second);
+        }
+        
+        return ans;
+    }
+};
