9b3955f986994f569937df0a7c23fcb7b99b43b7
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-07-25 19:37:34 +0530

    Update longest incr subsequ.cpp

--- Binary Search/longest incr subsequ.cpp
+++ Binary Search/longest incr subsequ.cpp
@@ -13,3 +13,34 @@ public:
         return s.size();
     }
 };
+
+// added another
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    vector<int> a(n);
+	    for(int i=0;i<n;i++)
+	        cin>>a[i];
+        int l=1;
+        vector<int> e(n);
+        e[0]=a[0];
+        
+        for(int i=1;i<n;i++){
+            auto it=lower_bound(e.begin(),e.begin()+l,a[i]);
+            if(it==e.begin()+l)
+                e[l++]=a[i];
+            else
+                *it=a[i];
+        }
+        cout<<l<<endl;
+	}
+	return 0;
+}
