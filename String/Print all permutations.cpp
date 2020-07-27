e70b828d18b67382d599139e686adc1e3b8d5be4
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-07-27 15:55:53 +0530

    Added string permutation without STL

--- String/Print all permutations.cpp
+++ String/Print all permutations.cpp
@@ -10,3 +10,14 @@ int main(){
     string s="hello";
     permutation(s);
 }
+
+// Without stl
+void f(string s,int l,int r){
+    if(l==r)
+        cout<<s<<" ";
+    for(int i=l;i<=r;i++){
+        swap(s[i],s[l]);
+        f(s,l+1,r);
+        swap(s[i],s[l]);
+    }
+}
