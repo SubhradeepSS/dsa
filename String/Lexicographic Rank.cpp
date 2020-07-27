96dd1f0b3ed8dce6da40f82b15baff82850414c3
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-07-27 18:34:28 +0530

    Added lexicographic string rank without STL

--- String/Lexicographic Rank.cpp
+++ String/Lexicographic Rank.cpp
@@ -16,3 +16,52 @@ int main(){
     string s="hello";
     lexicographic(s);
 }
+
+// without stl
+#include <iostream>
+#define m 1000003
+using namespace std;
+
+typedef long long int ll;
+
+ll fact(int n){
+    if(n<=1)
+        return 1;
+    return n*fact(n-1);
+}
+
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--){
+	    string s;
+	    cin>>s;
+	    int fl=0;
+	    int count[256]={0};
+	    for(int i=0;s[i];i++){
+	        count[s[i]]++;
+	        if(count[s[i]]>1){
+	            fl=1;
+	            break;
+	        }
+	    }
+	    if(fl==1){  // just for result=0 if repeating characters present
+	        cout<<0<<endl;
+	        continue; 
+	    }
+        for(int i=1;i<256;i++)
+            count[i]+=count[i-1];
+        int l=s.length();
+        ll f=fact(l);
+        ll ans=1;
+        for(int i=0;i<l;i++){
+            f/=l-i;
+            ans=(ans+((count[s[i]-1]%m)*(f%m))%m)%m;
+            for(int j=s[i];j<256;j++)
+                count[j]--;
+        }
+        cout<<ans<<endl;
+	}
+	return 0;
+}
