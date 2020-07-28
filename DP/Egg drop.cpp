af1e65aafd46617ba714c1a5d026c12d572aeb5c
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-07-28 19:31:03 +0530

    Egg drop - gfg [better] solution

--- DP/Egg drop.cpp
+++ DP/Egg drop.cpp
@@ -63,11 +63,9 @@ int superEggDrop(int K, int N) {
     }
 /*explanation:
 First, this is not an easy problem to understand. Before moving on, ask yourself how would you calculate the number of moves needed given 100 floors and 2 eggs
-
 A Natural Thinking Process: Assume we need x moves to find the egg breaking point in 100 floors with 2 eggs available. The floor you should start with given 2 eggs is the x-th floor. WHY? Becase if the first egg breaks on the x-th floor, you should try to drop the second egg from 1-th floor all the way to (x-1)-th floor to find the breaking point (note that in total you make x moves in worst case). On the other hand, if the first egg doesn't break at x-th floor, you should then try to drop it on (x+(x-1))-th floor. WHY? Because if the egg breaks on the (x+(x-1))-th floor, you would try to drop the second egg on (x+1)-th floor, and in total you will also make x moves in the worst case.
 
 With the above concept, how can we determin 'x' given 100 floor and 2 eggs ??? --> we want to solve the following formula: x + (x-1) + (x-2) + ... + 2 + 1 >= 100 --> x = 14. (this basic formula works for 2 eggs only, but it provides a crucial DP thinking foundation.)
-
 This gives us an idea: If arr is a M by K matrix, where M is the number of moves, K is the number of eggs, and arr[m][k] is the number of floors that we can make m moves with k eggs.
 
 Then, arr[m][k] = 1 + arr[m-1][k-1] + arr[m-1][k] ---> where '1 + arr[m-1][k-1]' is the exact floor that you should try to drop the first egg. WHY ??? If you drop the first egg higher than this floor and it breaks, then you are not guaranteed to find the breaking point. Because you have "m-1" moves and "k-1" eggs left, and you are in a position (strictly) higher than arr[m-1][k-1] floor.
@@ -91,3 +89,41 @@ def superEggDrop(self, K: int, N: int) -> int:
                 if dp[i][j] >= N:
                     return i
                     */
+
+
+// gfg concept of above - https://www.geeksforgeeks.org/eggs-dropping-puzzle-set-2/?ref=rp
+// C++ implementation of the approach 
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// Function to return the minimum number 
+// of trials needed in the worst case 
+// with n eggs and k floors 
+int eggDrop(int n, int k) 
+{ 
+	vector<vector<int> > dp(k + 1, vector<int>(n + 1, 0)); 
+
+	int x = 0; 
+
+	// Fill all the entries in table using 
+	// optimal substructure property 
+	while (dp[x][n] < k) { 
+
+		x++; 
+		for (int i = 1; i <= n; i++) 
+			dp[x][i] = dp[x - 1][i - 1] + dp[x - 1][i] + 1; 
+	} 
+
+	// Return the minimum number of moves 
+	return x; 
+} 
+
+// Driver code 
+int main() 
+{ 
+	int n = 2, k = 36; 
+
+	cout << eggDrop(n, k); 
+
+	return 0; 
+} 
