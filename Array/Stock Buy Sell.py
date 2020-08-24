6308c8d48e6887f923cc8dcfa0ee42a3fd8f577d
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-08-24 11:12:19 +0530

    Stock buy sell atmost 2 times

--- Array/Stock Buy Sell.py
+++ Array/Stock Buy Sell.py
@@ -15,3 +15,27 @@ def stock(arr,n):
     
 arr=[ 100, 180, 260, 310, 40, 535, 695 ]
 stock(arr,len(arr))
+
+# Buy sell at most 2 times
+"""
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<int> p(n,0);
+        int max_=prices[n-1];
+        for(int i=n-2;i>=0;i--){
+            if(prices[i]>max_)
+                max_=prices[i];
+            p[i]=max(p[i+1],max_-prices[i]);
+        }
+        int min_=prices[0];
+        for(int i=1;i<n;i++){
+            if(prices[i]<min_)
+                min_=prices[i];
+            p[i]=max(p[i-1],p[i]+prices[i]-min_);
+        }
+        return p[n-1];
+    }
+};
+"""
