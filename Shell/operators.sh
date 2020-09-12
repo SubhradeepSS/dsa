813938d66df837c24c9273a696fecaaf20ff5ae8
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-09-12 21:08:22 +0530

    fix

--- Shell/operators.sh
+++
@@ -1,9 +0,0 @@
-echo -n "Enter side of square: "
-read n
-echo "Area of square with side $n is $(($n*$n))"
-
-echo -n "Enter length of rectangle: "
-read l
-echo -n "Enter breadth of rectangle: "
-read b
-echo "Area of square with length=$l and breadth=$b is $(($l*$b))"
