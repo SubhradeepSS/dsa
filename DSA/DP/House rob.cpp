3493fe4eff80c016e0aebaa890500d602421bfe2
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-09-14 15:59:24 +0530

    add: O(1) space of house rob

--- DSA/DP/House rob.cpp
+++ DSA/DP/House rob.cpp
@@ -16,3 +16,19 @@ public:
         return a[n-1];
     }
 };
+
+// O(1) space
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if(nums.size()==0)
+            return 0;
+        int incl=nums[0],excl=0;
+        for(int i=1;i<nums.size();i++){
+            int old_excl=excl;
+            excl=max(incl,old_excl);
+            incl=max(incl,old_excl+nums[i]);
+        }
+        return max(excl,incl);
+    }
+};
