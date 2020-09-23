557fa458a4f910ea3b44c5bf83dd0052d1fcbe2e
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-09-23 18:03:09 +0530

    [update]: gas station with edge-case check

--- DSA/Stack and Queue/Petrol pump.cpp
+++ DSA/Stack and Queue/Petrol pump.cpp
@@ -31,3 +31,26 @@ int main()
   
     return 0;  
 } 
+
+
+// leetcode problem solution along with edge-case checking
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n=gas.size();
+        if(n==1)
+            return (gas[0] >= cost[0] ? 0 : -1);
+        int start=0,end=1,curr=gas[0]-cost[0];
+        while(start!=end || curr<0){
+            while(start!=end && curr<0){
+                curr -= gas[start] - cost[start];
+                start = (start+1)%n;
+                if(start == 0)
+                    return -1;
+            }
+            curr += gas[end] - cost[end];
+            end = (end+1)%n;
+        }
+        return start;
+    }
+};
