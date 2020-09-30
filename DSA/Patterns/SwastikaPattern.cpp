f6d32f49e80511df98635a46e261949817df9584
Warserpant122 <yashshah123boy@gmail.com>
2020-10-01 00:38:50 +0530

    [add] New subfolder Array/Patters

---
+++ DSA/Patterns/SwastikaPattern.cpp
@@ -0,0 +1,62 @@
+/*
+	This a CPP code to print pattern of a Swastika
+*/
+
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int n;
+cin>>n;
+    for (int i = 1; i <= n/2; i++)
+    {   cout<<"*";
+        for (int k = 0; k < (n/2)-1; k++)
+        {
+            cout<<" ";
+        }
+        cout<<"*";
+        if (i==1)
+        {
+            for (int k = 0; k<(n/2); k++)
+            {
+                cout<<"*";
+            }
+            
+        }
+        cout<<endl;
+        
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout<<"*";
+    }
+   cout<<endl;
+   for (int i = 0; i < n/2; i++)
+   {
+       if (i!=(n/2)-1)
+       {
+           for (int k = 0; k < (n/2); k++)
+           {
+               cout<<" ";
+           }
+           cout<<"*";
+       }
+       else
+       {
+           for (int k = 0; k <(n/2)+1; k++)
+           {
+               cout<<"*";
+           }
+           
+       }
+       for (int k = 0; k < (n/2)-1; k++)
+       {
+           cout<<" ";
+       }
+       cout<<"*"<<endl;
+   }
+    return 0;
+}
